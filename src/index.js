3e0a3add8d53ec5ff229a6650c950614ffc8e507
R-kjim <rkayjim@gmail.com>
2024-07-10 13:13:53 +0300

    DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputData=document.querySelector("#new-task-description")
+  const submitBtn=document.querySelector("input[type='submit']")
+  let myLists=document.querySelector("#tasks")
+  submitBtn.addEventListener('click',(e)=>{e.preventDefault(),printFn(e.target.inputData)})
+  function printFn(x){
+    let myLi=document.createElement("li")
+    myLi.textContent=inputData.value
+    myLists.append(myLi)
+    document.querySelector('form').reset()
+  }
 });
+
